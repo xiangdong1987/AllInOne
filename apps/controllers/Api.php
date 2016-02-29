93b7ed0eacce917853e9eabcaee3b09c8c84ad49
xiangdong <xiangdongdong@chelun.com>
2016-02-29 14:39:25 +0800

    1.文章管理完成

--- apps/controllers/Api.php
+++ apps/controllers/Api.php
@@ -9,57 +9,106 @@ class Api extends BasicController
     function __construct(\Swoole $swoole)
     {
         parent::__construct($swoole);
-        $this->is_ajax=true;
+        $this->is_ajax = true;
     }
+
     function login()
     {
-        $auto_login=false;
+        $auto_login = false;
         $this->session->start();
         $postData = file_get_contents("php://input");
-        $request = json_decode($postData,1);
-        if($request['RememberMe']){
-            $auto_login=true;
+        $request = json_decode($postData, 1);
+        if ($request['RememberMe']) {
+            $auto_login = true;
         }
-        $r = $this->user->login(trim($request['UserName']), $request['Password'],$auto_login);
+        $r = $this->user->login(trim($request['UserName']), $request['Password'], $auto_login);
         if ($r) {
             return $this->returnSucess("登陆成功");
         } else {
             return $this->returnFailure("登陆失败");
         }
     }
-    function addChapter(){
+
+    function addChapter()
+    {
         $postData = file_get_contents("php://input");
-        $request = json_decode($postData,1);
-        $data["chapter_name"]=$request["chapter_name"];
-        $data["status"]=$request["status"];
-        $data["pid"]=$request["pid"];
-        $data["rank"]=$request["rank"];
-        $chapter= model('Chapter');
-        $flag=$chapter->put($data);
+        $request = json_decode($postData, 1);
+        $data["chapter_name"] = $request["chapter_name"];
+        $data["status"] = $request["status"];
+        $data["pid"] = $request["pid"];
+        $data["rank"] = $request["rank"];
+        $chapter = model('Chapter');
+        $flag = $chapter->put($data);
         if ($flag) {
             return $this->returnSucess("插入成功");
         } else {
             return $this->returnFailure("插入失败");
         }
     }
-    function addArticle(){
+
+    function addArticle()
+    {
         $postData = file_get_contents("php://input");
-        $request = json_decode($postData,1);
-        $data["chapter_id"]=$request["chapter_id"];
-        $data["title"]=$request["title"];
-        $data["content"]=$request["content"];
-        $data["ctime"]=date('Y-m-d H:i:s',time());
-        $chapter= model('Article');
-        $flag=$chapter->put($data);
+        $request = json_decode($postData, 1);
+        $data["chapter_id"] = $request["chapter_id"];
+        $data["title"] = $request["title"];
+        $data["content"] = $request["content"];
+        $data["ctime"] = date('Y-m-d H:i:s', time());
+        $chapter = model('Article');
+        $flag = $chapter->put($data);
         if ($flag) {
             return $this->returnSucess("插入成功");
         } else {
             return $this->returnFailure("插入失败");
         }
     }
-    function getHomePage(){
-        $chapter= model('Article');
-        $data=$chapter->get('1',"id")->get();
+
+    function getArticle()
+    {
+        $postData = file_get_contents("php://input");
+        $request = json_decode($postData, 1);
+        $where["order"] = 'id';
+        $Article = model('Article');
+        $flag = $Article->gets($where);
+        if ($flag) {
+            return $this->returnSucess($flag);
+        } else {
+            return $this->returnFailure("插入失败");
+        }
+    }
+
+    function getArticleById()
+    {
+        $postData = file_get_contents("php://input");
+        $request = json_decode($postData, 1);
+        $id = $request['id'];
+        $chapter = model('Article');
+        $data = $chapter->get($id, "id")->get();
+        return $this->returnSucess($data);
+    }
+
+    function saveArticle()
+    {
+        $postData = file_get_contents("php://input");
+        $request = json_decode($postData, 1);
+        $id = $request['id'];
+        $data["chapter_id"] = $request["chapter_id"];
+        $data["title"] = $request["title"];
+        $data["content"] = $request["content"];
+        $data["ctime"] = date('Y-m-d H:i:s', time());
+        $chapter = model('Article');
+        $flag = $chapter->set($id, $data);
+        if ($flag) {
+            return $this->returnSucess($flag);
+        } else {
+            return $this->returnFailure("修改失败");
+        }
+    }
+
+    function getHomePage()
+    {
+        $chapter = model('Article');
+        $data = $chapter->get('1', "id")->get();
         return $this->returnSucess($data);
     }
 
--- build/controllers/admin/app.js
+++ build/controllers/admin/app.js
@@ -92,6 +92,7 @@ app.controller("HomeController", function ($scope, $location, $http, Authenticat
     $scope.headerUrl="./build/template/common/header.html?"+Date.parse(new Date());
     $scope.sidebarUrl="./build/template/common/sidebar.html?"+Date.parse(new Date());
     $scope.controlSidebarUrl="./build/template/common/control_sidebar.html?"+Date.parse(new Date());
+    $scope.articleList="";
     if(!AuthenticationService.isLoggedIn()){
         $location.path("/login");
     }
@@ -172,98 +173,38 @@ app.controller("HomeController", function ($scope, $location, $http, Authenticat
         $scope.settingTab='';
         $scope.statsTab='active';
     };
-});
-app.controller("ArticleController", function ($scope, $location, $http, AuthenticationService) {
-    $scope.credentials = {UserName: "", Password: "", RememberMe: false};
-    $scope.sidebar='';
-    $scope.userMenu='';
-    $scope.notify='';
-    $scope.homeTab='';
-    $scope.settingTab='active';
-    $scope.statsTab='';
-    $scope.tmplateUrl="./build/template/admin/index.html?"+Date.parse(new Date());
-    $scope.headerUrl="./build/template/common/header.html?"+Date.parse(new Date());
-    $scope.sidebarUrl="./build/template/common/sidebar.html?"+Date.parse(new Date());
-    $scope.controlSidebarUrl="./build/template/common/control_sidebar.html?"+Date.parse(new Date());
-    if(!AuthenticationService.isLoggedIn()){
-        $location.path("/login");
-    }
-    $scope.chapter = {chapter_name:"",status:1,pid:0,rank:0};
-    $scope.addChapter = function (chapter){
-        $http.post(config.home_url + "/api/addChapter", chapter).success(function(data){
-            console.log(data);
+    $scope.changePage = function (template){
+        if(template=='articleList'){
+            $http.post(config.home_url + "/api/getArticle").success(function(data){
+                if(data.code==1){
+                    console.log(data.data);
+                    $scope.articleList=data.data;
+                }else{
+                    $scope.articleList=data.message;
+                    $scope.showMessage=true;
+                }
+            });
+        }
+        $scope.tmplateUrl="./build/template/admin/"+template+".html?"+Date.parse(new Date());
+    };
+    $scope.editArticle = function (id){
+        $http.post(config.home_url + "/api/getArticleById",{id:id}).success(function(data){
+            $scope.article.title=data.data.title;
+            $scope.article.chapter_id=data.data.chapter_id;
+            $scope.article.content=data.data.content;
+            $scope.article.id=data.data.id;
+        });
+        $scope.tmplateUrl="./build/template/admin/editArticle.html?"+Date.parse(new Date());
+    };
+    $scope.saveArticle = function(article){
+        $http.post(config.home_url + "/api/saveArticle",article).success(function(data){
             if(data.code==1){
-                $location.path("/index");
+                $scope.changePage('articleList');
             }else{
-                $scope.message=data.message;
+                $scope.articleList=data.message;
                 $scope.showMessage=true;
             }
         });
-    };
-    $scope.article ={chapter_id:"",title:"",content:""};
-    $scope.addArticle = function (chapter){
-        $http.post(config.home_url + "/api/addArticle", chapter).success(function(data){
-            console.log(data);
-            if(data.code==1){
-                $location.path("/index");
-            }else{
-                $scope.message=data.message;
-                $scope.showMessage=true;
-            }
-        });
-    };
-
-    $scope.logout = function () {
-        AuthenticationService.logout().success(function (data) {
-            console.log(data);
-            $location.path("/login");
-        });
-    };
-    $scope.sidebarToggle = function () {
-        if(window.innerWidth>config.screenSizes.sm-1){
-            if($scope.sidebar){
-                $scope.sidebar='';
-            }else{
-                $scope.sidebar='sidebar-collapse';
-            }
-        }else{
-            if($scope.sidebar){
-                $scope.sidebar='';
-            }else{
-                $scope.sidebar='sidebar-open';
-            }
-        }
-    };
-    $scope.controlSidebarToggle = function () {
-        if($scope.controlSidebar){
-            $scope.controlSidebar='';
-        }else{
-            $scope.controlSidebar='control-sidebar-open';
-        }
-    };
-    $scope.userMenuToggle = function (){
-        if($scope.userMenu){
-            $scope.userMenu='';
-            $scope.ariaExpanded='false';
-        }else{
-            $scope.userMenu='open';
-            $scope.ariaExpanded='true';
-        }
-    };
-    $scope.homeTabToggle = function(){
-        $scope.homeTab='active';
-        $scope.settingTab='';
-        $scope.statsTab='';
-    };
-    $scope.settingTabToggle = function(){
-        $scope.homeTab='';
-        $scope.settingTab='active';
-        $scope.statsTab='';
-    };
-    $scope.statsTabToggle = function(){
-        $scope.homeTab='';
-        $scope.settingTab='';
-        $scope.statsTab='active';
     };
 });
 
